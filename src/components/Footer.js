7adc41c12d18cfb9fe66594432175c5f79983cda
martin karugaba <martinkarugaba21@gmail.com>
2022-08-24 23:41:15 +0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return <Wrapper>
+    <h5>
+      &copy; {new Date().getFullYear()} Confy Sloth. All rights reserved.
+    </h5>
+  </Wrapper>;
+};
 
 const Wrapper = styled.footer`
   height: 5rem;
@@ -26,6 +31,6 @@ const Wrapper = styled.footer`
   @media (min-width: 776px) {
     flex-direction: row;
   }
-`
+`;
 
-export default Footer
+export default Footer;
