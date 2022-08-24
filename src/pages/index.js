543bcfd66bff59692216e72007ba8d5009adc923
martin karugaba <martinkarugaba21@gmail.com>
2022-08-24 03:04:37 +0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import Private from './PrivateRoute';
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  Private,
+};
