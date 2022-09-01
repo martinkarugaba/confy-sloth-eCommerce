1a1f44a28d6b7df00408ea9ed3d670be040a6dea
martin karugaba <martinkarugaba21@gmail.com>
2022-09-01 05:46:45 +0300

    added stars - manual approach

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -44,7 +44,7 @@ const SingleProductPage = () => {
     price,
     description,
     stock,
-    starts,
+    stars,
     reviews,
     id: sku,
     company,
@@ -62,7 +62,7 @@ const SingleProductPage = () => {
           <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className="price">{formatPrice(price)}</h5>
             <p className="description">{description}</p>
             <p className="info">
